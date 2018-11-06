5cec000c38f0fb3818c26bcc55cccd17a9feb134
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-11-06 12:51:50 -0600

    added validation check to users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation
+const validationRegisterInput = require("../../validation/register");
+
 //Load User model
 const User = require("../../models/User");
 
@@ -18,9 +21,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //@description  Register user
 //@access       Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validationRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exsists" });
+      errors.email = "Email already exsists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
