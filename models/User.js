2951a50d1f6a8d7bd5818eb1451cc6e71c3a5519
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-11-12 15:31:31 -0600

    fixed variable error

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-mongoose = require("mongoose");
-Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //Create Schema
 const UserSchema = new Schema({
